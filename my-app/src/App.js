9d97011999ba47de75dde7e89c41bbb379a3b3a5
Long Nguyen - PC <ntl.284@gmail.com>
2020-12-02 08:34:00 +0700

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import GlassShop from "./Components/GlassShop";
 
 function App() {
   return (
-    <div className="App">
-      ABC
+    <div>
+      <GlassShop />
     </div>
   );
 }
---
+++ my-app/src/Components/GlassShop.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class GlassShop extends Component {
+
+  render() {
+      
+    return (
+      <div className="">
+        <div className="container-fluid body__background text-center">
+          <h5 className="text-white">TRY GLASSES APP ONLINE</h5>
+        </div>
+      </div>
+    );
+  }
+}

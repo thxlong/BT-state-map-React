77fb01de9ca664e8c4180ef3b0873bc0628ef318
Long Nguyen - PC <ntl.284@gmail.com>
2020-12-03 22:49:44 +0700

    add layout

--- my-app/src/Components/GlassShop.js
+++ my-app/src/Components/GlassShop.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 export default class GlassShop extends Component {
-
   render() {
-      
+    var imgBG = "./img/background.jpg";
+    var imgBackground = {
+      width: "100%",
+      height: "768px",
+      backgroundImage: `url(${imgBG})`,
+      backgroundRepeat: "repeat",
+    };
+
     return (
-      <div className="">
-        <div className="container-fluid body__background text-center">
-          <h5 className="text-white">TRY GLASSES APP ONLINE - BÀI LÀM CHƯA XONG ANH ƠI!</h5>
+      <div
+        className="container-fluid body__background text-center px-0"
+        style={imgBackground}
+      >
+        <div className="header__bgColor container-fluid">
+          <h4 className="text-white">TRY GLASSES APP ONLINE</h4>
+        </div>
+
+        <div className="content">
+          <div className="model1"></div>
+          <div className="model2"></div>
         </div>
       </div>
     );
